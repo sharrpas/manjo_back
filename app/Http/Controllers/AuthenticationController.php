4fea7805493d867b7aa2f5c774535d3b506ef587
macbook <sharrpas@gmail.com>
2025-12-13 12:33:29 +0330

    [Add]: verify phone

--- app/Http/Controllers/AuthenticationController.php
+++ app/Http/Controllers/AuthenticationController.php
@@ -2,13 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Constants\Status;
+use App\Http\Requests\VerifyPhoneRequest;
 use App\Models\User;
 use App\Traits\GeneratesUniqueUidTrait;
+use App\Traits\PhoneValidator;
 use Illuminate\Http\Request;
 
 class AuthenticationController extends Controller
 {
-    use GeneratesUniqueUidTrait;
+    use GeneratesUniqueUidTrait, PhoneValidator;
 
     public function login()
     {
@@ -16,6 +19,38 @@ class AuthenticationController extends Controller
         $user = User::query()->create([
             'username' => $username,
         ]);
+
+        return $this->success([
+            'username' => $user->username,
+            'token' => $user->createToken(
+                name: 'token_base_name',
+                expiresAt: now()->addMinutes(config('sanctum.expiration')))
+                ->plainTextToken,
+            'expiration' => (config('sanctum.expiration')/24/60) . ' days',
+        ]);
+    }
+
+    public function verifyPhone(VerifyPhoneRequest $request)
+    {
+        $validateOTP = $this->validate($request->phone,$request->otp);
+        if (!$validateOTP){
+            return $this->error(Status::PERMISSION_DENIED,'کد تایید اشتباه است');
+        }
+
+        /** @var User $cacheUser */
+        $cacheUser = auth()->user();
+        $user = User::query()->where('phone', $request->phone)->first();
+
+        if (!$user){
+            $cacheUser->update([
+                'phone' => $request->phone,
+                'phone_verified_at' => now(),
+            ]);
+            $user = $cacheUser;
+        }else{
+            $user->gameRecords()->update(['user_id' => $cacheUser->id]);
+            $cacheUser->delete();
+        }
 
         return $this->success([
             'username' => $user->username,
--- routes/api.php
+++ routes/api.php
@@ -8,3 +8,4 @@ use Illuminate\Support\Facades\Route;
 Route::post('send-otp', [OtpController::class,'store']);
 
 Route::post('login',[AuthenticationController::class,'login']);
+Route::post('verify-phone',[AuthenticationController::class,'verifyPhone'])->middleware('auth:sanctum');
