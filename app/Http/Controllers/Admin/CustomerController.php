a083f0d5491dc2b292e42b8bc1b58345fbe5fd73
macbook <sharrpas@gmail.com>
2025-12-22 13:41:58 +0330

    [Add]: customers in admin

--- app/Http/Controllers/Admin/CustomerController.php
+++ app/Http/Controllers/Admin/CustomerController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\UserCollection;
+use App\Http\Resources\UserShowResource;
 use App\Models\User;
 use Illuminate\Http\Request;
 
@@ -13,5 +14,11 @@ class CustomerController extends Controller
     {
         $users = User::query()->orderBy('phone_verified_at','DESC');
         return $this->success((new UserCollection($users->paginate(10))));
+    }
+
+    public function show($username)
+    {
+        $user = User::where('username',$username)->firstOrFail();
+        return $this->success(UserShowResource::make($user));
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Admin\AdminAuthenticationController;
+use App\Http\Controllers\Admin\CustomerController;
 use App\Http\Controllers\AuthenticationController;
 use App\Http\Controllers\OtpController;
 use App\Http\Controllers\ScoreboardController;
@@ -8,21 +9,22 @@ use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::post('send-otp', [OtpController::class,'store']);
+Route::post('send-otp', [OtpController::class, 'store']);
 
-Route::post('login',[AuthenticationController::class,'login']);
-Route::post('verify-phone',[AuthenticationController::class,'verifyPhone'])->middleware('auth:sanctum');
+Route::post('login', [AuthenticationController::class, 'login']);
+Route::post('verify-phone', [AuthenticationController::class, 'verifyPhone'])->middleware('auth:sanctum');
 
-Route::get('profile',[UserController::class,'getProfile'])->middleware('auth:sanctum');
+Route::get('profile', [UserController::class, 'getProfile'])->middleware('auth:sanctum');
 
 Route::get('scoreboard', [ScoreboardController::class, 'index']);
 
 
-Route::post('admin-login',[AdminAuthenticationController::class,'login']);
+Route::post('admin-login', [AdminAuthenticationController::class, 'login']);
 
-Route::prefix('admin')->group(function () {
+Route::prefix('admin')->middleware(['auth:sanctum', 'role:admin'])->group(function () {
 
-    Route::middleware('auth:sanctum','admin')->group(function () {
-        Route::get('customers',[\App\Http\Controllers\Admin\CustomerController::class,'index']);
-    });
+    Route::get('customers', [CustomerController::class, 'index']);
+    Route::get('customers/{username}', [CustomerController::class, 'show']);
+
+
 });
